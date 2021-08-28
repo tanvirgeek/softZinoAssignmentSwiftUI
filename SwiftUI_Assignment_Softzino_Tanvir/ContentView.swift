1bc71d82d376f8a617ec0c854a165ecdf38b7bfb
tanvirgeek <tanvirgeek@gmail.com>
2021-08-28 07:06:34 +0600

    Initial Commit

---
+++ SwiftUI_Assignment_Softzino_Tanvir/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI_Assignment_Softzino_Tanvir
+//
+//  Created by MD Tanvir Alam on 28/8/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI_Assignment_Softzino_Tanvir/SwiftUI_Assignment_Softzino_TanvirApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Assignment_Softzino_TanvirApp.swift
+//  SwiftUI_Assignment_Softzino_Tanvir
+//
+//  Created by MD Tanvir Alam on 28/8/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Assignment_Softzino_TanvirApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
